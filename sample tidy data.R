5363ca9f030a078d7604b30237dc4a007e64573a
vindication09 <vharo00@gmail.com>
2018-02-27 10:20:12 -0500

    Update sample tidy data.R

--- sample tidy data.R
+++ sample tidy data.R
@@ -15,14 +15,22 @@ refdom<-c("yahoo","NULL","fb", "Twitter", "NULL", "#g45y6", "Yahoo","ggggggggg",
 df.refdom<-data.frame(as.character(refdom))
 df.refdom
 
-sample.df<-data.frame(df.x,df.progroups,df.country, df.refdom)
+FinanceHits<-c("12","45", "89", "0", 'NULL',"20","21", "NULL", "NULL", "67")
+PoliticsHits<-c("34","89","398","473", "2342", '90833',"57", "NULL","--", "7745")
+NewsHits<-c("44","4982", "8749","4990","62804", "90023", "8933", "NULL", "9032","103270")
+df.hits<-data.frame(FinanceHits, PoliticsHits, NewsHits)
+df.hits
+
+sample.df<-data.frame(df.x,df.progroups,df.country, df.refdom, df.hits)
 sample.df
 
 colnames(sample.df)[colnames(sample.df)=="as.character.x."] <- "visitorID"
 colnames(sample.df)[colnames(sample.df)=="as.character.progroups."] <- "ProGroup"
 colnames(sample.df)[colnames(sample.df)=="as.character.country."] <- "Country"
 colnames(sample.df)[colnames(sample.df)=="as.character.refdom."] <- "ReferralDomain"
-head(sample.df)
+head(sample.df) 
+
+write.csv(sample.df, "C:/Users/traveler/Desktop/tidysample.csv")
 
 
 
